bacb9b4560d1a8fd79a8aabde57a623bf08df77f
shurizzle <shura1991@gmail.com>
2021-06-03 03:04:43 +0200

    Re-export figfont objects

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,11 @@ mod utils;
 pub use crate::figure::FIGure;
 pub use crate::line::FIGline;
 
-// TODO: re-rexports
+pub use figfont::*;
+
+mod prelude {
+    pub use super::error::Error;
+    pub use super::result::Result;
+    pub use super::FIGfont;
+    pub use super::FIGure;
+}
